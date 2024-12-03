643fd4bd935492542fa078880c1586461346c733
Shreyas <shreyas314159@gmail.com>
2024-12-03 23:12:20 +0000

    added the right asset path which is /vite-dev and also updated the vite.config.ts which had some corrections

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
 
 export default defineConfig({
-  plugins: [
   ssr: {
-      noExternal: true,
+    noExternal: true,
   },
-    react(),
-    RubyPlugin(),
-  ],
-})
+  plugins: [react(), RubyPlugin()],
+});
